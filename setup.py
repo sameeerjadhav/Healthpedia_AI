9beecf4a1f2a5ae1376079261e42d57e7d201302
sameeerjadhav <sameerjadhav581@gmail.com>
2024-01-12 20:19:04 -0500

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Healthpedia AI',
+    version= '0.0.0',
+    author= 'Sameer Narendra Jadhav',
+    author_email= 'sameerjadhav581@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
